db08127cd1f5a142bdc20159dc7bad766b47815a
Mdhabib10 <129630008+Mdhabib10@users.noreply.github.com>
2023-08-09 17:26:55 -0400

    Add files via upload

---
+++ Atomic-project/public_html/script.js
@@ -0,0 +1,17 @@
+
+
+
+document.getElementById('buyButton').addEventListener('click', function () {
+    
+    window.location.href = 'payment.html';
+});
+
+document.getElementById('viewMore').addEventListener('click', function () {
+   
+    window.location.href = 'viewmore.html';
+});
+
+document.getElementById('back').addEventListener('click', function () {
+    
+    window.location.href = 'home.html';
+});
